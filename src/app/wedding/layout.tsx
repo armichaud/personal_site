fe12ddbca379e9cdde504803478e887526b3c889
Alexander Rodion Michaud <alexanderrodionmichaud@Alexanders-MacBook-Pro.local>
2023-08-10 22:17:00 -0400

    Working on getting favicon working

---
+++ src/app/wedding/layout.tsx
@@ -0,0 +1,19 @@
+export const metadata = {
+    title: "Andrea and Alexander's Wedding",
+    description: 'May 23, 2023 in Tuscany, Italy',
+}
+
+export default function WeddingLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
+  
