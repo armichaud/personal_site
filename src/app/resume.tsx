24f0a6f63d7e8754a5036cf246f9428b01757a61
Alexander Rodion Michaud <alexanderrodionmichaud@Alexanders-MacBook-Pro.local>
2023-08-11 19:22:23 -0400

    Starting point for resume

---
+++ src/app/resume.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import RESUME_DATA from "./resume_data";
+
+export interface JobExperienceProps {
+  jobTitle: string;
+  company: string;
+  startDate: string;
+  endDate: string;
+  description: string;
+}
+
+const JobExperience: React.FC<JobExperienceProps> = ({
+  jobTitle,
+  company,
+  startDate,
+  endDate,
+  description,
+}) => (
+    <div className="p-4 border rounded-md shadow-md mb-4">
+        <h2 className="text-xl font-semibold">{jobTitle}</h2>
+        <p className="text-gray-600">
+            {company} | {startDate} - {endDate}
+        </p>
+        <p className="mt-2">{description}</p>
+    </div>
+);
+
+
+const Resume = (): JSX.Element => {
+  return (
+    <div className="container mx-auto py-8">
+      <h1 className="text-2xl font-semibold mb-4">My Resume</h1>
+      <div>
+        {RESUME_DATA.map((experience, index) => (
+          <JobExperience key={index} {...experience} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Resume;
---
+++ src/app/resume_data.ts
@@ -0,0 +1,5 @@
+import { JobExperienceProps } from "./resume";
+
+const RESUME_DATA: JobExperienceProps[] = [];
+
+export default RESUME_DATA;
