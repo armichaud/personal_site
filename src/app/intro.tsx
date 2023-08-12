255248c8d8578b8d65c477a9f79dba0d68fb4495
Alexander Rodion Michaud <alexanderrodionmichaud@Alexanders-MacBook-Pro.local>
2023-08-11 22:50:05 -0400

    Name

--- src/app/intro.tsx
+++ src/app/intro.tsx
@@ -5,7 +5,8 @@ import React from 'react';
 const Intro: React.FC = () => {
     return (
         <div className="flex flex-col items-center justify-center h-full">
-        <h1 className="text-4xl font-semibold mb-4">Welcome to my website!</h1>
+        <h1 className="text-4xl font-semibold mb-4">Alexander Rodion Michaud</h1>
+        <h2>or ARM, if you're in a hurry</h2>
         <img className="rounded-lg" src="https://imagedelivery.net/Xh-8qVol1Pftdals16ieKg/ccbbd565-53b4-4780-7fee-b4bcef64c100/public" alt="profile_pic" />
         <p className="text-gray-600 text-center">
             This is a website I built to showcase my coding projects and résumé.
