e9ee1b0fde0286465cd10b6c1fa63fa159579d31
armichaud <email@alexandermichaud.tech>
2023-11-14 02:57:35 -0500

    Try outer dimensions

--- src/app/StarsField.tsx
+++ src/app/StarsField.tsx
@@ -13,9 +13,9 @@ const StarsField = ({ children }: StarsFieldProps) => {
   useEffect(() => {
     if (typeof window !== 'undefined') {
         const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const camera = new THREE.PerspectiveCamera(75, window.outerWidth / window.outerHeight, 0.1, 1000);
         const renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setSize(window.outerWidth, window.outerHeight);
         containerRef?.current?.appendChild(renderer.domElement);
 
         const starsGeometry = new THREE.BufferGeometry();
@@ -35,8 +35,8 @@ const StarsField = ({ children }: StarsFieldProps) => {
         
         const mouse = new THREE.Vector2();
         document.addEventListener('mousemove', (event) => {
-          mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-          mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+          mouse.x = (event.clientX / window.outerWidth) * 2 - 1;
+          mouse.y = -(event.clientY / window.outerHeight) * 2 + 1;
         });
 
         window.addEventListener('deviceorientation', (event: DeviceOrientationEvent) => {
@@ -56,8 +56,8 @@ const StarsField = ({ children }: StarsFieldProps) => {
         animate();
 
         const handleResize = () => {
-            const newWidth = window.innerWidth;
-            const newHeight = window.innerHeight;
+            const newWidth = window.outerWidth;
+            const newHeight = window.outerHeight;
 
             camera.aspect = newWidth / newHeight;
             camera.updateProjectionMatrix();
@@ -68,12 +68,12 @@ const StarsField = ({ children }: StarsFieldProps) => {
         window.addEventListener('resize', handleResize);
         window.addEventListener('tabChanged', handleResize);
 
-        // Clean up event listeners on component unmount
         return () => {
             window.removeEventListener('resize', handleResize);
+            window.removeEventListener('tabChanged', handleResize);
         };
     }
-  }, []); // Empty dependency array ensures useEffect runs once on mount
+  }, []);
 
   return (
     <div className="relative w-full h-full" >
