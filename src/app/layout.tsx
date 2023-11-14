41f8644b65b90e7635cfe664504b3cb21b6fc37a
armichaud <email@alexandermichaud.tech>
2023-11-14 02:11:49 -0500

    Try just adding a second viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Amiri"></link>
         <SchemaMarkup />
       </head>
