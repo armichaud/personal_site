e87ef81f1faddcd5ec79d475a93e591557dde4a9
armichaud <email@alexandermichaud.tech>
2023-11-14 04:14:36 -0500

    Border only on dark mode

--- src/app/intro.tsx
+++ src/app/intro.tsx
@@ -45,7 +45,7 @@ const Intro: React.FC = () => {
             <p className='mb-2'>or ARM, if you&apos;re in a hurry</p>
             <div className='flex p-4 justify-between align-center flex-col sm:flex-row max-h-[600px] mt-6'>
                 <div className='sm:w-1/2'>
-                    <img className="rounded-lg border-double border-4 border-gray-600 max-h-[100%]" src="https://imagedelivery.net/Xh-8qVol1Pftdals16ieKg/ccbbd565-53b4-4780-7fee-b4bcef64c100/public" alt="profile_pic" /> 
+                    <img className="rounded-lg dark:border-solid dark:border-4 dark:border-gray-600 max-h-[100%]" src="https://imagedelivery.net/Xh-8qVol1Pftdals16ieKg/ccbbd565-53b4-4780-7fee-b4bcef64c100/public" alt="profile_pic" /> 
                 </div>
                 <section className="p-6 sm:w-1/2 flex flex-col justify-around mt-12 sm:mt-0">
                     <div className='flex flex-col justify-center mb-14 sm:mb-6'>
