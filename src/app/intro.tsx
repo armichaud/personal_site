dcddd1a250b3512396c692f2ab3be870b9a4dfe7
armichaud <email@alexandermichaud.tech>
2024-06-07 23:14:09 -0400

    Better styling

--- src/app/intro.tsx
+++ src/app/intro.tsx
@@ -46,26 +46,26 @@ const CompanyIconsRow: React.FC = () => {
 const Intro: React.FC = () => {
     return (
         <div className="flex flex-col items-center justify-center h-full">
-            <h1 className="text-4xl font-semibold mb-1 text-center">Alexander Rodion Michaud</h1>
-            <p className='mb-2'>or ARM, if you&apos;re in a hurry</p>
-            <div className='flex p-4 justify-between align-center flex-col sm:flex-row max-h-[600px] mt-6'>
-                <div className='sm:w-1/2'>
-                    <img className="rounded-lg dark:border-solid dark:border-4 dark:border-gray-600 max-h-[100%]" src="https://imagedelivery.net/Xh-8qVol1Pftdals16ieKg/ccbbd565-53b4-4780-7fee-b4bcef64c100/public" alt="profile_pic" /> 
+            <h1 className="text-3xl sm:text-5xl lg:text-6xl font-semibold mb-1 text-center">Alexander Rodion Michaud</h1>
+            <p className='mb-2 sm:text-2xl'>or ARM, if you&apos;re in a hurry</p>
+            <div className='flex p-4 align-center flex-col sm:flex-row mt-6'>
+                <div className="rounded-lg sm:w-1/2 md:relative md:overflow-hidden">
+                    <img className="rounded-lg md:absolute md:-top-[20%] lg:-top-[30%] xl:-top-[50%] w-[100%]" src="https://imagedelivery.net/Xh-8qVol1Pftdals16ieKg/ccbbd565-53b4-4780-7fee-b4bcef64c100/public" alt="profile_pic" /> 
                 </div>
-                <section className="p-6 sm:w-1/2 flex flex-col justify-around mt-12 sm:mt-0">
-                    <div className='flex flex-col justify-center mb-14 sm:mb-6'>
-                        <h2 className="text-2xl lg:text-3xl font-bold mb-1 text-center ">Where I&apos;ve Worked</h2> 
+                <section className="px-8 xl:px-12 py-0 sm:w-1/2 flex flex-col mt-14 sm:mt-0">
+                    <div className='flex flex-col justify-center mb-12 sm:mb-8'>
+                        <h2 className="text-2xl lg:text-3xl font-bold mb-1 text-center">Where I&apos;ve Been</h2> 
                         <CompanyIconsRow />
                     </div>
-                    <div className='flex flex-col justify-center mb-14 sm:mb-4'>
+                    <div className='flex flex-col justify-center mb-12 sm:mb-8'>
                         <h2 className="text-2xl lg:text-3xl font-bold mb-1 text-center">What I&apos;ve Done</h2>
-                        <p className='text-center'>
-                          Developed apps for the web (NextJS, Ruby on Rails, PHP) and Android (Java/Kotlin). 
+                        <p className='text-center lg:text-lg'>
+                          Developed apps for the web (NextJS, Ruby on Rails, PHP) and Android (Java/Kotlin), led QA devs at a SaaS startup, and managed the notifications backend at an ecommerce site. 
                         </p>
                     </div>
-                    <div className='flex flex-col justify-center mb-14 sm:mb-6'>
-                        <h2 className="text-2xl lg:text-3xl font-bold mb-1 text-center">What I&apos;m Doing Now</h2>
-                        <p className='text-center'>Using Python-based compliance tools to ensuring Capital One's AWS resources comply with safety standards</p>
+                    <div className='flex flex-col justify-center'>
+                        <h2 className="text-2xl lg:text-3xl font-bold mb-1 md:mb-2 text-center">What I&apos;m Doing</h2>
+                        <p className='text-center lg:text-lg'>Automating safety compliance checks for AWS resources in Python with <a className="text-orange-500 hover:underline" href="https://cloudcustodian.io">Cloud Custodian</a>.</p>
                     </div>
                 </section>
             </div>
