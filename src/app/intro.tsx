0ca29bda0df9ee0b8b6ad5d2b0434de3cad9f95d
armichaud <email@alexandermichaud.tech>
2024-06-08 00:59:19 -0400

    Fix scroll speed

--- src/app/intro.tsx
+++ src/app/intro.tsx
@@ -3,47 +3,6 @@
 import React from 'react';
 import LogoCarousel from './logo_carousel';
 
-const CompanyIconsRow: React.FC = () => {
-    const companies = [
-      {
-        name: "Capital One",
-        logo: <img className="max-h-24 w-24" src="https://tbcdn.talentbrew.com/company/234/gst_v1_0/img/logo-capital-one.svg" alt="c1"/>,
-        website: "https://www.capitalone.com",
-      },
-      {
-        name: "Drizly",
-        logo: <img className="max-h-24" src="https://imagedelivery.net/Xh-8qVol1Pftdals16ieKg/7698f546-d965-4a5d-e766-2200beb85100/public" alt="drizly"></img>,
-        website: "https://www.drizly.com",
-      },
-      {
-        name: "ProdPerfect",
-        logo: <img className="max-h-24 scale-75" src="https://prodperfect.com/wp-content/themes/prodperfect/favicon.png" alt='p2' />,
-        website: "https://prodperfect.com",
-      },
-      {
-        name: "Wayfair",
-        logo: <img className="max-h-24" src="https://imagedelivery.net/Xh-8qVol1Pftdals16ieKg/6855f684-027c-4252-5bbc-b4b711a1fd00/public" alt="wayfair" />,
-        website: "https://www.wayfair.com",
-      },
-    ];
-  
-    return (
-      <div className="flex justify-center items-center space-x-4">
-        {companies.map((company, index) => (
-            <a
-                key={index}
-                href={company.website}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:opacity-75 transition-opacity"
-            >
-                {company.logo}
-            </a>
-        ))}
-      </div>
-    );
-  };
-
 const Intro: React.FC = () => {
     return (
         <div className="flex flex-col items-center justify-center h-full">
--- src/app/logo_carousel.tsx
+++ src/app/logo_carousel.tsx
@@ -3,22 +3,22 @@ import React, { useState, useEffect, useRef } from 'react';
 const companies = [
     {
       name: "Capital One",
-      logo: <img className="max-h-24 w-24" src="https://tbcdn.talentbrew.com/company/234/gst_v1_0/img/logo-capital-one.svg" alt="c1"/>,
+      logo: <img className="max-h-24 w-24 scale-[90%]" src="https://tbcdn.talentbrew.com/company/234/gst_v1_0/img/logo-capital-one.svg" alt="c1"/>,
       website: "https://www.capitalone.com",
     },
     {
       name: "Drizly",
-      logo: <img className="max-h-24" src="https://imagedelivery.net/Xh-8qVol1Pftdals16ieKg/7698f546-d965-4a5d-e766-2200beb85100/public" alt="drizly"></img>,
+      logo: <img className="max-h-24" src="https://imagedelivery.net/Xh-8qVol1Pftdals16ieKg/7698f546-d965-4a5d-e766-2200beb85100/public" alt="drizly"/>,
       website: "https://www.drizly.com",
     },
     {
       name: "ProdPerfect",
-      logo: <img className="max-h-24 scale-75" src="https://prodperfect.com/wp-content/themes/prodperfect/favicon.png" alt='p2' />,
+      logo: <img className="max-h-24 scale-75" src="https://prodperfect.com/wp-content/themes/prodperfect/favicon.png" alt='p2'/>,
       website: "https://prodperfect.com",
     },
     {
       name: "Wayfair",
-      logo: <img className="max-h-24" src="https://imagedelivery.net/Xh-8qVol1Pftdals16ieKg/6855f684-027c-4252-5bbc-b4b711a1fd00/public" alt="wayfair" />,
+      logo: <img className="max-h-24" src="https://imagedelivery.net/Xh-8qVol1Pftdals16ieKg/6855f684-027c-4252-5bbc-b4b711a1fd00/public" alt="wayfair"/>,
       website: "https://www.wayfair.com",
     },
   ];
@@ -35,7 +35,8 @@ const LogoCarousel = () => {
         const animate = () => {
             if (!isPaused && carouselRef.current) {
                 let current = (carouselRef.current as HTMLDivElement);
-                current.scrollLeft += 1;
+                if (animationFrameId % 4 === 0) current.scrollLeft += 1;
+                // current.scrollLeft += 1;
                 if (current.scrollLeft >= current.scrollWidth / 2) {
                     current.scrollLeft = 0;
                 }
@@ -56,12 +57,12 @@ const LogoCarousel = () => {
     return (
         <div
             ref={carouselRef}
-            className="flex justify-center items-center overflow-x-hidden relative w-[75%] sm:w-full md:w-[75%] lg:w-[60%] xl:w-[50%]"
+            className="flex space-x-4 overflow-x-hidden relative w-[75%] sm:w-full md:w-[75%] lg:w-[60%] xl:w-[50%]"
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
         >
             <div
-                className="flex transition-transform items-center space-x-4"
+                className="flex transition-transform items-center"
                 style={{ width: `${allCompanies.length * 100}%` }}
             >
                 {allCompanies.map((company, index) => (
