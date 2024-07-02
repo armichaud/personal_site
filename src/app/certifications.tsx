86310c699cf2da63130e842d235c23a01fa63274
armichaud <email@alexandermichaud.tech>
2024-07-02 10:43:27 -0400

    Terraform cert

--- src/app/certifications.tsx
+++ src/app/certifications.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+const dimensions = {
+  "data-iframe-width": "250",
+  "data-iframe-height": "300",
+};
+
 const Certifications = (): JSX.Element => {
   return (
     <div>
@@ -7,17 +12,29 @@ const Certifications = (): JSX.Element => {
         <h1 className="text-2xl font-semibold mb-2">Certifications</h1>
         <p>The code behind embedded badges is controlled by the certification issuer and does not work consistently. If badges do not display properly, reload the page.</p>
       </div>
-      <div className="mt-8">
-        <div 
-          data-iframe-width="150" 
-          data-iframe-height="270" 
-          data-share-badge-id="4acf7de6-b12e-4bda-ae42-7c524d073708" 
-          data-share-badge-host="https://www.credly.com" 
-        />
-        <script 
-          type="text/javascript" 
-          async src="//cdn.credly.com/assets/utilities/embed.js" 
-        />
+      <div className="flex flex-col space-y-4 sm:space-y-0 sm:space-x-8 justify-center items-center sm:flex-row sm:justify-start mt-8">
+        <div id="solutions-architect">
+          <div 
+            {...dimensions}
+            data-share-badge-id="4acf7de6-b12e-4bda-ae42-7c524d073708" 
+            data-share-badge-host="https://www.credly.com" 
+          />
+          <script 
+            type="text/javascript" 
+            async src="//cdn.credly.com/assets/utilities/embed.js" 
+          />
+        </div>
+        <div id="terraform">
+          <div 
+            {...dimensions}
+            data-share-badge-id="ac5b8711-c0aa-49b9-9d8e-0c9090860a28" 
+            data-share-badge-host="https://www.credly.com"
+          />
+          <script 
+            type="text/javascript" 
+            async src="//cdn.credly.com/assets/utilities/embed.js"
+          />
+        </div>
       </div>
     </div>
   );
