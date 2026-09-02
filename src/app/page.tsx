318605fa9aefca214695135593707fcc0458a3e9
armichaud <email@alexandermichaud.tech>
2026-09-02 10:31:46 -0400

    Localize profile pic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import pic from './assets/outside_profile_pic.jpeg';
+import Image from 'next/image';
+
 const Main = (): JSX.Element => (
     <div className="p-4 xl:p-12">
         <div className="rounded-lg relative object-cover">
-            <img 
+            <Image
                 className="rounded-lg absolute top-6 right-0 sm:right-5 w-[25%] xl:w-[33%] xl:-top-[45%] xl:right-12 xl:top-6 object-contain" 
-                src="https://imagedelivery.net/Xh-8qVol1Pftdals16ieKg/ccbbd565-53b4-4780-7fee-b4bcef64c100/public" 
+                src={pic}
                 alt="profile_pic"
             /> 
         </div>
